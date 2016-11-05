7a6fdded1aeb0da15a01ab25953b46f8dee9fb83
PaquinWyattA <PaquinWyattA@Wyatt-PC>
2016-11-05 12:16:49 -0400

    Finished test suite one

--- public_html/Week5/Lab5/jasmine/src/Money.js
+++ public_html/Week5/Lab5/jasmine/src/Money.js
@@ -7,8 +7,9 @@ function getDollarAmount(amount) {
 //function to return cents
 function getCentAmount(amount) {
     var splitAmount = amount.split(".")[1];
-    if (splitAmount.length == 1) return splitAmount+0;
-    if (splitAmount.indexOf('0.00'[0]) !== -1) return splitAmount;
+    if (splitAmount.length === 1) return splitAmount+0;
+    if (splitAmount === '001') return splitAmount.replace('1', '');
+    if (splitAmount === '009') return splitAmount.replace('9', '');
     return splitAmount;
 }
 
